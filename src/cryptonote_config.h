b1c340923984cd2d24153c07846542dd6122fa02
valiant1x <29789261+valiant1x@users.noreply.github.com>
2018-04-17 13:05:02 -0400

    block major v4 changes

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -1,12 +1,15 @@
 #pragma once
 
 #define CRYPTONOTE_PUBLIC_ADDRESS_TEXTBLOB_VER          0
-#define CURRENT_TRANSACTION_VERSION                     1
-#define CURRENT_BLOCK_MAJOR_VERSION                     3
+#define CURRENT_TRANSACTION_VERSION                     2
+#define CURRENT_BLOCK_MAJOR_VERSION                     4
 #define CURRENT_BLOCK_MINOR_VERSION                     0
 
 #define BLOCK_MAJOR_VERSION_1                           1
-#define BLOCK_MAJOR_VERSION_2                           3
+#define BLOCK_MAJOR_VERSION_2                           2
+#define BLOCK_MAJOR_VERSION_3                           3
+#define BLOCK_MAJOR_VERSION_4                           4
+
 
 #define COIN                                            ((uint64_t)100000000) // pow(10, 8)
 #define DEFAULT_FEE                                     ((uint64_t)100000) // pow(10, 5)
