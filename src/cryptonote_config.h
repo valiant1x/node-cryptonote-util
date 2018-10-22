3cabc00a2437d0388df4acde86e1ea4bdf6c62e0
valiant1x <valiant1x@users.noreply.github.com>
2018-10-22 01:34:09 -0500

    fix block version typo

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -9,7 +9,7 @@
 #define BLOCK_MAJOR_VERSION_2                           2
 #define BLOCK_MAJOR_VERSION_3                           3
 #define BLOCK_MAJOR_VERSION_4                           4
-#define BLOCK_MAJOR_VERSION_4                           5
+#define BLOCK_MAJOR_VERSION_5                           5
 
 
 #define COIN                                            ((uint64_t)100000000) // pow(10, 8)
